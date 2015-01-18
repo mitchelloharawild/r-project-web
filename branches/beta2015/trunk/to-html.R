ea2c45317dc77b20ba80037a329a4ccb19839ab3
urbanek <urbanek@1153ebb8-59df-0310-8e53-ad3d7429db60>
2015-01-18 19:44:27 +0000

    Include navbar in template
    From: hadley <h.wickham@gmail.com>
    
    git-svn-id: https://svn.r-project.org/R-project-web@890 1153ebb8-59df-0310-8e53-ad3d7429db60

--- branches/beta2015/trunk/to-html.R
+++ branches/beta2015/trunk/to-html.R
@@ -7,8 +7,9 @@ dirs <- files[file.info(file.path("md", files))$isdir]
 lapply(file.path("out", dirs), dir.create, showWarnings = FALSE)
 
 # convert markdown files
+system("pandoc md/navbar.md -o out/navbar.html")
 conv <- paste0("pandoc ", file.path("md", md), " -o ", file.path("out", gsub("md", "html", md)), 
-  " --template template.html")
+  " --template template.html --include-before out/navbar.html")
 lapply(conv, system)
 
 # copy non-markdown files
