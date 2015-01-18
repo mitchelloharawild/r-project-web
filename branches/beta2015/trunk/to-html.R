e9293e6d4db212603da09bb27b1d0cb98ba3e927
urbanek <urbanek@1153ebb8-59df-0310-8e53-ad3d7429db60>
2015-01-18 19:44:26 +0000

    Render md to html
    From: hadley <h.wickham@gmail.com>
    
    git-svn-id: https://svn.r-project.org/R-project-web@887 1153ebb8-59df-0310-8e53-ad3d7429db60

---
+++ branches/beta2015/trunk/to-html.R
@@ -0,0 +1,15 @@
+files <- dir("md", recursive = TRUE, include.dirs = TRUE)
+is_md <- grepl("\\.md$", files)
+md <- files[is_md]
+
+dir.create("out")
+dirs <- files[file.info(file.path("md", files))$isdir]
+lapply(file.path("out", dirs), dir.create, showWarnings = FALSE)
+
+# convert markdown files
+conv <- paste0("pandoc ", file.path("md", md), " -o ", file.path("out", gsub("md", "html", md)))
+lapply(conv, system)
+
+# copy non-markdown files
+other <- files[!is_md]
+file.copy(file.path("md", other), file.path("out", other))
